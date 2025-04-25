7cc37a425b8aca40a2838096eab44a0c0903a161
Dhruvesh05 <160326003+Dhruvesh05@users.noreply.github.com>
2025-04-25 22:21:53 +0530

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
-import AppRoutes from "./config/routes.jsx";
+import AppRoutes from "./config/routes.jsx"; // Ensure this path is correct
 import { Toaster } from "react-hot-toast";
 import { ChatProvider } from "./context/ChatContext.jsx";
-import Routes from "./src/config/Routes.jsx";
-
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
