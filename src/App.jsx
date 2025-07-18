d3e7061fbd5ce63649ae12bed4c96a5c60722c90
Dhruvesh Patil <160326003+Dhruvesh05@users.noreply.github.com>
2025-07-19 00:22:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import React, { useEffect } from "react";
-import { connectSocket, disconnectSocket } from "./services/socket";
+import JoinCreateChat from "./components/JoinCreateChat";
 
 function App() {
-  useEffect(() => {
-    connectSocket(); // connect when component mounts
-
-    return () => {
-      disconnectSocket(); // cleanup on unmount
-    };
-  }, []);
-
   return (
     <div>
-      <h1>🧠 C3ube Chat App</h1>
-      <p>Check the console for WebSocket activity.</p>
+      <JoinCreateChat />
     </div>
   );
 }
